ea2cddf86629dec306d79e6339225f5625e24a8e
MindofSkk <helpmeskk@gmail.com>
2022-12-14 14:10:36 +0530

    todo-ready

--- app/src/components/Todo.jsx
+++ app/src/components/Todo.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 import { useEffect } from "react";
 export const Todo = () => {
   const [data, setData] = useState("");
+  const [todo, setTodo] = useState([]);
 
-  
   const add = () => {
     console.log("hello");
     console.log(data);
@@ -18,8 +18,8 @@ export const Todo = () => {
         task: data,
       })
       .then((res) => {
-        alert("Data post Sucessfully");
-
+        // alert("Data post Sucessfully");
+        getdata();
       })
       .catch((err) => {
         console.log(err);
@@ -27,15 +27,30 @@ export const Todo = () => {
   };
 
   useEffect(() => {
+    getdata();
+  }, []);
+
+  const getdata = () => {
     axios
       .get("http://localhost:8080/todo")
       .then((res) => {
-      console.log("res",res.data)
+        // console.log("res",res.data);
+        setTodo(res.data);
+        console.log(todo);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  };
+
+  const deltetodo = (id) => {
+    axios
+      .delete(`http://localhost:8080/todo/${id}`)
+      .then((res) => {
+        getdata()
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
@@ -49,6 +64,20 @@ export const Todo = () => {
         <button className={styles.btn} onClick={add}>
           Add Task
         </button>
+      </div>
+
+      <div className={styles.table}>
+        {todo.map((e, index) => {
+          return (
+            <div key={index}>
+              <p className={styles.todotext}> 🎯 {e.task}</p>
+              <button onClick={() => deltetodo(e.id)} className={styles.deltebtn}>
+                {" "}
+                Delete ❌
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
